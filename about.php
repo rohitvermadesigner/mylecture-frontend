f9bf24a63ba9c4ad090dc5c41b6ec0f3e6f732c8
Rohit Verma <verma6641@gmail.com>
2022-04-23 16:43:00 +0530

    about us content

--- about.php
+++ about.php
@@ -9,25 +9,22 @@
 
     <section class="common-section">
         <div class="container">
-            <h2 class="title-primary">About <span>Us</span> </h2>
-        </div>
-    </section>
-
-    <section class="common-section pt-0">
-        <div class="container">
             <div class="row">
+                <div class="col-md-12 mb-4">
+                    <h2 class="title-primary">About <span>Us</span> </h2>
+                </div>
                 <div class="col-md-6">
                     <img src="assets/images/next-pg.jpg" alt="" class="img-thumbnail" />
                 </div>
                 <div class="col-md-6">
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        GEMS next ( Global education of medical sciences) believes in providing qualitative as well as quantitative solutions to NEXT aspirants in order to enhance their preparation for the examination.
                     </p>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        The relevant & most important part of pre- & para-clinical subjects will be covered through National Exit Test: NEXT point of view.
                     </p>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        Gems next provides an edge to its students over others with comprehensive and meticulously designed study material that gives students conceptual clarity and command over the most difficult topics with easy explanations, simple examples and multiple practice questions.Question bank of more than one lakh questions on all subjects & topics.
                     </p>
                 </div>
             </div>
@@ -41,17 +38,29 @@
             <div class="row">
                 <div class="col-md-6">
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        We have an in-house R&D Team which revises and updates the study material,question bank on regular intervals in line with the recent changes in the pattern of the examination.
                     </p>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        Organize all your lectures & study material effectively so you can access them from anywhere.
                     </p>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+                        Mapping of your weaknesses & strengths in view of the National Exit Test: NEXT pattern. Our team will help you in converting your weaknesses into your strengths.
+                    </p>
+                    <p>
+                        Our package & courses are on the current pattern of NEXT Exam.
                     </p>
                 </div>
                 <div class="col-md-6">
                     <img src="assets/images/next-pg.jpg" alt="" class="img-thumbnail" />
+                </div>
+                <div class="col-md-12 mt-4">
+                    <p>
+                        At gems, we ensure the regular assessment of performance by providing assignments and generation of real time reports and you can practice mock tests on a regular basis and keep a constant check on the performance of every student individually.
+                    </p>
+                    <p>
+                        Just give us 30 minutes daily as per your convenience & we will ensure that you crack NEXT with flying colors!
+                    </p>
+
                 </div>
             </div>
 
