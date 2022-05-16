4a3dce7692dbd786dbc49a1ef944450df4595cdc
Rohit Verma <verma6641@gmail.com>
2022-05-16 22:47:13 +0530

    next page contnt & images updated

--- next.php
+++ next.php
@@ -10,22 +10,112 @@
     <section class="common-section">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 mb-4">
-                    <h2 class="title-primary">About <span>NEXT</span> </h2>
-                </div>
                 <div class="col-md-6">
-                    <img src="assets/images/about1.jpg" alt="" class="img-thumbnail" />
-                </div>
-                <div class="col-md-6">
+                    <h2 class="title-primary">National Exit Test <span>NEXT</span> </h2>
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                        NEXT (more commonly known as the National Exit Exam for MBBS) is a single window entrance examination for all MBBS students in India that is required to pass the final year and obtain a license to practice medicine. NEXT will also serve the purpose of Screening Test for foreign medical graduates (Foreign Medical Graduates Examination or FMGE) &for admission into postgraduate medical courses.
                     </p>
                     <p>
-                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        Students who successfully clear the NEXT exam will be able to start their internship. Scores (rank) in the NEXT exit exam will determine the allotment of seats for PG courses (MD/MS/DNB/Diploma) in government and private medical colleges in India. It will standardize medical education. It will also judge the conceptual understanding and problem solving skills of final year MBBS students.
                     </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="assets/images/next1.jpg" alt="" class="img-thumbnail" />
                 </div>
             </div>
 
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <img src="assets/images/next2.png" alt="" class="img-fluid" />
+                </div>
+                <div class="col-md-6">
+                    <h2 class="title-primary">NEXT EXAM <span>PATTERN</span> </h2>
+                    <p>The exam will comprise of TWO parts :</p>
+                    <p>
+                        NEXT Part -1: Online MCQ Based Exam <br>
+                        NEXT Part -2: Practical Based Exam
+                    </p>
+                    <p>
+                        <b>1. THEORY (NExT-1)</b><br>
+                        Theory questions will be multiple choice questions (MCQs). It will be conducted after the final year MBBS, before the internship. After passing the NEXT-1 students will begin their internship. The score of the NEXT-1 exam will be valid for 2 to 3 years for the purpose of PG Entrance ranking.Those who manage to pass theory (i.e. NEXT-1) will have to clear NEXT-2, the practical test, separately at the end of internship.
+                    </p>
+
+                    <p>
+                        <b> 2. PRACTICAL (NExT-2)</b><br>
+                        NExT-2 will be a pass or fail practical exam which will be conducted by college/university. In order to qualify it, you should have knowledge of basic practical skills. These skills will check the practical efficacy of students.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <h2 class="title-primary">NEXT EXAM <span>SYLLABUS </span> </h2>
+                    <p>
+                        NEXT-1 will have a total of 540 questions. It will be conducted for 3 days over 14.5 hours i.e 4.5 hours per day divided between Pre-Lunch and Post-Lunch.
+                    </p>
+                    <h4>APPROX CONTENT SPLIT : -</h4>
+                    <p>
+                        60% Problem Solving <br>
+                        30% Comprehension and Analysis<br>
+                        10% Recall<br>
+                    </p>
+
+                    <div class="table table-responsive">
+                        <table class="table-bordered w-100">
+                            <thead>
+                                <tr>
+                                    <th>Exam</th>
+                                    <th>Major Subjects</th>
+                                    <th>Allied Subjects</th>
+                                    <th>MCQ<span>s</span></th>
+                                    <th>Subject</th>
+                                    <th>MCQ<span>s</span></th>
+                                    <th>MCQ<span>s</span></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Day 1</td>
+                                    <td>Medicine</td>
+                                    <td>Dermatology Phychiatry</td>
+                                    <td>120</td>
+                                    <td>Pediatrics</td>
+                                    <td>60</td>
+                                    <td>180</td>
+                                </tr>
+                                <tr>
+                                    <td>Day 2</td>
+                                    <td>Surgery</td>
+                                    <td>Orthopedics Anethesia</td>
+                                    <td>120</td>
+                                    <td>ENT</td>
+                                    <td>60</td>
+                                    <td>180</td>
+                                </tr>
+                                <tr>
+                                    <td>Day 3</td>
+                                    <td>Objective & Gyneology</td>
+                                    <td>Radiology</td>
+                                    <td>120</td>
+                                    <td>Opthalmology</td>
+                                    <td>60</td>
+                                    <td>180</td>
+                                </tr>
+                                <tr class="font-weight-bold">
+                                    <td colspan="3">Total</td>
+                                    <td>360</td>
+                                    <td></td>
+                                    <td>180</td>
+                                    <td>540</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     </section>
