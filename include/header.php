1b716da52c68c93e8b17adae39b8edbf52efea11
Rohit Verma <verma6641@gmail.com>
2022-05-22 17:14:06 +0530

    added career page

--- include/header.php
+++ include/header.php
@@ -61,6 +61,7 @@
                 <!-- <li><a href="live-lectures.php">Live Lectures</a></li> -->
                 <li><a href="mock-test.php">Mock Test</a></li>
                 <li><a href="book-demo-class.php">Book Demo Class</a></li>
+                <li><a href="career.php">Career</a></li>
                 <li><a href="contact.php">Contact</a></li>
             </ul>
         </div>
