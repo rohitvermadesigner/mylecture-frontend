61494e45f3102716c495fda9a3b7b26fe4ae967e
Rohit Verma <verma6641@gmail.com>
2022-08-02 00:00:08 +0530

    return policy content

--- cancellation-policy.php
+++ cancellation-policy.php
@@ -11,7 +11,28 @@
     <section class="product-section">
         <div class="container">
         <h2 class="title-primary text-center">Cancellation <span>Policy</span></h2>
-           
+           <p>
+           The services of this website gemsnext.com (GlobalNext Education of Medical Sciences) are available only to the registered users on a single user basis, for a particular period of time on making the stipulated payment and abiding by the applicable terms and conditions. The said services are personal in nature and cannot be assigned or transferred to or shared with any other person other than the registered users.
+           </p>
+           <p>
+           By registering yourself as a user and subscribing to avail the services of this website gemsnext.com  it will be deemed that you have read, understood and agreed to fully abide by all the terms and conditions of the website as contained herein.
+           </p>
+           <p>
+           The right to use the services of the website GlobalNext Education of Medical Sciences is on revocable license / permission basis as per the terms and conditions contained herein. Except for the availability of the services during the license period, the registered users shall not have any right or title over the website or any of its contents.
+           </p>
+           <p>
+           The services offered by GlobalNext Education of Medical Sciences through this site are listed in detail in the home page of this site.
+           </p>
+           <p>
+           GlobalNext Education of Medical Sciences currently provides users with access to a rich collection/compilation of online educational information and related resources etc. As a user of this site, you understand and agree that all the services provided are on the basis of as-it-is and GlobalNext Education of Medical Sciences assume no liability for the accuracy or completeness or use of, nor any liability to update, the information contained on this website.
+           </p>
+           <p>
+           In order to use GlobalNext Education of Medical Sciences online services, you must obtain access to the World Wide Web, either directly or through devices that access web-based content, and pay any service fees associated with such access. In addition, you must provide all equipment necessary to make such a connection to the World Wide Web, including a computer and modem or other access devices.
+           </p>
+           <h4>Cancellation and Refund Policy</h4>
+           <p>
+           Please note that Packages once subscribed cannot be cancelled and no refund to that effect can be made. Since gemsNext.com is offering non-tangible irrevocable goods, we do not issue refunds once the package is subscribed. As a customer you are responsible for understanding that no refund or cancellation is possible upon any subscription at our site.
+           </p>
         </div>
     </section>
     <!-- product-section ends here -->
